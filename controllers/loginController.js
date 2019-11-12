7b0235107cc3a4f3e5537a53cf126016f5a7035a
PavalSidhu <pavalsidhu@hotmail.com>
2019-11-12 13:41:17 -0800

    fix commit

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,11 +1,22 @@
 let loginModel = require('../models/loginData');
 
-<<<<<<< HEAD
-exports.getLogin = (req, res) => {
-    res.render('detailsPage');
+exports.userLogin = (req, res) => {
+    res.render('login', { loginCSS: true });
 }
-=======
-exports.userLogin = (req,res) =>{
-    res.render('login',{loginCSS:true});
+
+exports.postDetails = (req, res, next) => {
+    let a_imageURL = req.body.img_Url;
+    let a_about = req.body.about;
+    let a_country = req.body.country;
+    let a_dob = req.body.dob;
+
+    let detailsObject = {
+        img_URL: a_imageURL,
+        about: a_about,
+        country: a_country,
+        dob: a_dob
+    }
+
+    artistModel.add(detailsObject);
+    res.redirect(301, '/login');
 }
->>>>>>> 3a39d40f788b363d0b156020d8990d4adcc60619
--- routes/myRoutes.js
+++ routes/myRoutes.js
@@ -2,11 +2,10 @@ const express = require('express');
 const loginController = require('../controllers/loginController');
 
 const router = express.Router();
-<<<<<<< HEAD
 
-router.get('/', loginController.getLogin);
-=======
 router.get("/login",loginController.userLogin)
->>>>>>> 3a39d40f788b363d0b156020d8990d4adcc60619
+
+router.post('/submitDetails', loginController.postDetails);
+
 
 module.exports = router;
