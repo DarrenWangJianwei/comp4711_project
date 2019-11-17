dd6bfb31f57112a4ea87fefa4b3fc072199f82d4
darre <Darren.wjy1990@gmail.com>
2019-11-17 12:22:54 -0800

    add space on each if statment main-layout

--- routes/myRoutes.js
+++ routes/myRoutes.js
@@ -7,25 +7,25 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-router.get("/login",loginController.userLogin)
-
+router.get("/login",loginController.userLogin);
+router.get("/detailsPage", loginController.userDetailsPage);
+router.post('/submitDetails', loginController.postDetails);
 
 router.get("/",homeController.index);
-router.get("/user/:user_id",homeController.userMain)
-router.post("/user/post",homeController.userPost)
-router.post("/user/discussion",homeController.LastestDiscussion)
-router.post("/user/discussion/reply", homeController.LastestDiscussionReplies)
-router.post("/user/discussion/reply/submit", homeController.postReply)
+router.get("/user/:user_id",homeController.userMain);
+router.post("/user/post",homeController.userPost);
+router.post("/user/discussion",homeController.LastestDiscussion);
+router.post("/user/discussion/reply", homeController.LastestDiscussionReplies);
+router.post("/user/discussion/reply/submit", homeController.postReply);
 
-router.get("/user/:user_id/posts", postController.getAllPostByUser)
-router.post("/user/search/subject", searchController.getSubjectPost)
-router.get("/user/:user_id/edit", userController.index)
+router.get("/user/:user_id/posts", postController.getAllPostByUser);
+router.post("/user/search/subject", searchController.getSubjectPost);
+router.get("/user/:user_id/edit", userController.index);
 
-router.get("/user/:user_id/view/:user", userController.index)
-router.post("/user/likes", userController.increaseLikes)
-router.get("/detailsPage", loginController.userDetailsPage);
+router.get("/user/:user_id/view/:user", userController.index);
+router.post("/user/likes", userController.increaseLikes);
 
-router.post('/submitDetails', loginController.postDetails);
+
 
 
 module.exports = router;
